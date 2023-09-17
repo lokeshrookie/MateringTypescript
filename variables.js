3956d2819c6676c8dfda11bdd064f13656bb2d08
lokeshrookie <rocklinglokesh@gmail.com>
2023-09-17 19:28:24 +0530

    init

---
+++ variables.js
@@ -0,0 +1,15 @@
+//---------------looops--------------------------------
+var students = ["lokesh", "rakesh", "ram", "prudhvi"];
+// for(let index = 0; index<students.length; index++){
+//     console.log(students[index]);
+// }
+var index = 0;
+while (index < students.length) {
+    console.log(students[index]);
+    index++;
+}
+// ------------------------------------
+// var title: string = "The secret"
+// let author: string = "rhando burne"
+// const price: number = 390.50;
+// console.log("the book " + title+ " with price  is: " + price + " is written by: " + author);
---
+++ variables.ts
@@ -0,0 +1,20 @@
+//---------------looops--------------------------------
+var students: string[] = ["lokesh", "rakesh", "ram", "prudhvi"];
+
+let dynamic: any = 66;
+console.log(dynamic)
+
+
+
+
+
+
+
+// ------------------------------------
+// var title: string = "The secret"
+// let author: string = "rhando burne"
+// const price: number = 390.50;
+
+
+// console.log("the book " + title+ " with price  is: " + price + " is written by: " + author);
+ 
